3da9a0a5ce89ffe8611a35ca2ec61d75aaaef4c8
npage1129 <82468860+npage1129@users.noreply.github.com>
2022-02-28 10:07:41 -0700

    commit tree 2b038d533e8ad1509345a719c9abd49df9b40382

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution here
+// // Code your solution here
+// function findMatching(array, string){ 
+// let result = [];
+// for(let driver of array){
+//   if(string(driver) === true){
+//     result = driver;
+//     break;
+//   }
+// }
+// return result;
+// }
+// findMatching.filter(drivers);
+// findMatching.filter(drivers.name);
+//   function superbowlWin(array){
+//     let conclusion = undefined
+//     for(const game of array){
+//         if(game.result === "W"){
+//              conclusion = game.year;
+//             break;
+
+//         }
+
+//     }
+//    return conclusion; 
+// }
+// superbowlWin.find(record);
+
+function findMatching(array, string){
+  const result = array.filter(word => word.toLowerCase() === string.toLowerCase());
+  return result;
+}  
+  
+function fuzzyMatch(array, string){
+  const result = array.filter(letters => letters.slice(0,2) === string.slice(0,2));
+  return result;
+}
+function matchName(driver, string){
+  const result = driver.filter(word => word.name === string);
+    return result;
+  }
+
+  
+     
+
+  
